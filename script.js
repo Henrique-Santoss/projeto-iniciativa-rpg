7929071641e7e58b3dd1b106cebdffcaccde0acb
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-08-01 21:00:32 -0400

    Começo do script

---
+++ script.js
@@ -0,0 +1,14 @@
+var nome = document.querySelector('input#inome');
+var cor = document.querySelector('input#icor');
+var ini = document.querySelector('input#iini')
+
+var person = []
+
+function enviar(){
+    person.push()
+    resultado.innertext()
+}
+
+function limpar(){
+
+}
