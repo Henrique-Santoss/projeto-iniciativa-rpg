0431fbd0faf5e316944e2c0ec3f84c1d13d5ac1b
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-08-02 18:06:58 -0400

    Atualização botão Enviar

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-var nome = document.querySelector('input#inome');
-var cor = document.querySelector('input#icor');
+var nome = document.querySelector('input#inome')
+var cor = document.querySelector('input#icor')
 var ini = document.querySelector('input#iini')
-
+var lista = document.querySelector('select#plista')
+var res = document.querySelector('output#ordemIni')
 var person = []
+var lperson = []
 
 function enviar(){
-    person.push()
-    resultado.innertext()
+    if (nome.value === ''){
+        alert("Por favor digite os dados corretamente !!!")
+    } else {
+        person.push(nome.value, cor.value, ini.value)
+        let item = document.createElement('option')
+        item.text = (`${nome.value}`)
+        lista.appendChild (item)
+        nome.value = ''
+        cor.value = ''
+        ini.value = ''
+    }
+    lperson.push(person)
+    res.innerHTML = (`${lperson}`)
 }
 
 function limpar(){
-
+    lista.innerHTML = ''
 }
